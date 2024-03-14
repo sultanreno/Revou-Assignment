dcc41c6e9a71a9b29ed18354a26314949ab7c92b
sultanreno <sultanardiansyah2726@gmail.com>
2024-03-14 20:14:27 +0700

    add js feture

---
+++ js/script.js
@@ -0,0 +1,117 @@
+var variable ='A';
+let letible ='B';
+const constanta ='C';
+var arraydata = [];
+
+// looping for
+for (let index = 0; index <= 5; index++) { 
+    console.log('index->' + index);
+}
+
+// looping while
+
+let i = 0;
+while (i <= 5) {
+    console.log(i);
+    i++;
+}
+
+// looping do while
+
+let j = 0;
+do {
+    console.log(j);
+    j++;
+} while (j <= 5);
+
+// looping break
+
+let k = 0;
+do {
+    console.log(k);
+    k++;
+} while (k <= 5);
+
+// looping continue
+
+let l = 0;
+do {
+    console.log(l);
+    l++;
+} while (l <= 5);
+
+// looping switch
+
+let m = 0;
+
+switch (m) {
+    case 0:
+        console.log(m);
+        break;
+    case 1:
+        console.log(m);
+        break;
+    case 2:
+        console.log(m);
+        break;
+    case 3:
+        console.log(m);
+        break;
+    case 4:
+        console.log(m);
+        break;
+    case 5:
+        console.log(m);
+        break;
+    default:
+        console.log(m);
+        break;
+}
+
+// looping switch case
+ let n = 0;
+
+switch (n) {
+    case 0:
+        console.log(n);
+        break;
+    case 1:
+        console.log(n);
+        break;
+    case 2:
+        console.log(n);
+        break;
+    case 3:
+        console.log(n);
+        break;
+    case 4:
+        console.log(n);
+        break;
+    case 5:
+        console.log(n);
+        break;
+    default:
+        console.log(n);
+        break;
+}
+
+// ini standard
+if (variable == 'A') {
+    console.log(variable);
+}
+
+// tanpa tanda kurung 
+if (letible == 'B') console.log(letible);
+
+// operator if
+constanta == 'D'
+  ? console.log(constanta)
+  : console.log('bukan C');
+  
+// operator if else
+
+const a = 10;
+const b = 20;
+const c = 30;
+
+a > b
